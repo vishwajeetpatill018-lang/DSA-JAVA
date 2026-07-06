3cb6ea6ce5ddc5110d956cd61bc02fa2db600846
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-07-06 13:26:46 +0530

    Time: 0 ms (100%), Space: 44.2 MB (67.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,16 +10,46 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode f = null;
-        ListNode curr = head;
-        ListNode prev = null;
-
-        while(curr != null){
-            f = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = f;
+        
+        if(head == null)
+         return null;
+        ArrayList<ListNode> ans = new ArrayList<>();
+        ListNode temp = head;
+        while(temp != null){
+            // Add All nodes in ArrayList
+            ans.add(temp);
+            temp = temp.next;
         }
-        return prev;
+        // Reverse Traversal and make connection
+        int n = ans.size();
+        for(int i = n-1; i>=1; i-- ){
+            ans.get(i).next = ans.get(i-1);
+        }
+        ans.get(0).next = null;
+        return ans.get(n-1);
+
+        // ListNode f = null;
+        // ListNode curr = head;
+        // ListNode prev = null;
+
+        // while(curr != null){
+        //     f = curr.next;
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = f;
+        // }
+        // return prev;
+
+        // if(head == null || head.next == null){
+        //     return head;
+        // }
+        // ListNode a = head.next;
+        // ListNode newHead = head;
+
+        
+        // head.next = null;
+        // newHead = reverseList(a);
+        // a.next = head;
+        // return newHead;
     }
 }
