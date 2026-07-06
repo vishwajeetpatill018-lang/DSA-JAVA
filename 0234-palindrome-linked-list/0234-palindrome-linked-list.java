19e554231fdaea37f7544b9107bc7fafcbad2809
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-07-06 17:36:07 +0530

    Time: 3 ms (99.83%), Space: 94.6 MB (44.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,25 +11,59 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-        ArrayList<Integer> ans = new ArrayList<>();
-        ListNode temp = head;
-        while(temp != null){
-            ans.add(temp.val);
-            temp = temp.next;
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     ans.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // int n = ans.size();
+        // int i =0;
+        // int j = n-1;
+        // while(i<j){
+        //     // int a = ans.get(i);
+        //     // int b = ans.get(j);
+        //     // if(a!=b)
+        //     if(!ans.get(i).equals(ans.get(j)))
+        //     {
+        //         return false;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+
+        // Approach 2 : slow/fast + Reverse 2nd Half
+        // Space : O(n);
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int n = ans.size();
-        int i =0;
-        int j = n-1;
-        while(i<j){
-            // int a = ans.get(i);
-            // int b = ans.get(j);
-            // if(a!=b)
-            if(!ans.get(i).equals(ans.get(j)))
-            {
+
+        // Reverse 2nd Half
+
+        ListNode prev = null;
+        ListNode fwd = null;
+        ListNode curr = slow;
+
+        while(curr != null){
+            fwd = curr.next; // save next Node
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        // Check if equal
+        ListNode i = head;
+        ListNode j = prev;
+
+        while(j != null){
+            if(i.val != j.val){
                 return false;
             }
-            i++;
-            j--;
+            i = i.next;
+            j = j.next;
         }
         return true;
     }
