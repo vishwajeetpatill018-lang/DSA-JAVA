6a66b9de31ba603eae28ebe46d8374df020551e1
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-07-18 10:48:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
@@ -1,19 +1,33 @@
+// class Solution {
+//     public static void reverseStack(Stack<Integer> st) {
+        
+//         Stack<Integer> st2 = new Stack<>();
+//         while(st.size() > 0 ){
+//             st2.push(st.pop());
+//         }
+//         Stack<Integer> st3 = new Stack<>();
+        
+//         while(st2.size() >0 ){
+//             st3.push(st2.pop());
+//         }
+        
+//         while(st3.size() > 0){
+//             st.push(st3.pop());
+//         }
+        
+//         // T.C & S.C = O(n)
+        
+//     }
+// }
 class Solution {
     public static void reverseStack(Stack<Integer> st) {
-        
-        Stack<Integer> st2 = new Stack<>();
-        while(st.size() > 0 ){
-            st2.push(st.pop());
+
+        Stack<Integer> temp = new Stack<>();
+
+        while (!st.isEmpty()) {
+            temp.push(st.pop());
         }
-        Stack<Integer> st3 = new Stack<>();
-        
-        while(st2.size() >0 ){
-            st3.push(st2.pop());
-        }
-        
-        while(st3.size() > 0){
-            st.push(st3.pop());
-        }
-        
+
+        st.addAll(temp);
     }
 }
