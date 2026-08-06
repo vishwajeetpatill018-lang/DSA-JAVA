3896b0425a1527e346bd0b3c038a4e6c6a3c3301
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-08-06 23:42:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Queue Reversal/queue-reversal.java
+++ Difficulty: Easy/Queue Reversal/queue-reversal.java
@@ -1,6 +1,9 @@
 class Solution {
     public void reverseQueue(Queue<Integer> q) {
         
+        // Push all elements in stack
+        // Add all elements in queue
+        
         Stack<Integer> st = new Stack<>();
         
         while(q.size() >0){
