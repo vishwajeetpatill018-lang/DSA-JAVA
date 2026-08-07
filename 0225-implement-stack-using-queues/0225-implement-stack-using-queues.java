7d9c500fb0fe0dfcfb99cb03f46f4a5b1c266d12
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-08-07 22:58:52 +0530

    Time: 1 ms (79.25%), Space: 43 MB (34.2%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -3,33 +3,68 @@ class MyStack {
     public MyStack() {
         
     }
-    
-    public void push(int x) {
-        q.add(x);
-    }
-    
-    public int pop() {
-        int n = q.size();
-        for(int i=0; i<n-1; i++){
-            q.add(q.remove());
-        }
-        return q.remove();
-    }
-    
-    public int top() {
-        int n = q.size();
-        for(int i =0; i<n-1; i++){
-            q.add(q.remove());
-        }
-        int p = q.peek();
-        q.add(q.remove());
 
-        return p;
-    }
+    //PUSH EFFICIENT APPROACH =>
+    
+    // public void push(int x) {
+    //     // O(1)
+    //     q.add(x);
+    // }
+    
+    // public int pop() {
+
+    //     // O(n) time and Space
+    //     int n = q.size();
+    //     for(int i=0; i<n-1; i++){
+    //         q.add(q.remove());
+    //     }
+    //     return q.remove();
+    // }
+    
+    // public int top() {
+    //      // O(n) time and Space
+    //     int n = q.size();
+    //     for(int i =0; i<n-1; i++){
+    //         q.add(q.remove());
+    //     }
+    //     int p = q.peek();
+    //     q.add(q.remove());
+
+    //     return p;
+    // }
     
     public boolean empty() {
         return (q.isEmpty());
     }
+
+
+    //POP EFFICIENT APPROACH
+
+    public void push(int x){
+        // I will add elements in Queue at front of the queue by remove and adding first n-1 elements
+
+        q.add(x);
+        int n = q.size();
+        for(int i =0; i<n-1; i++){
+            q.add(q.remove());
+        }
+       
+
+    }
+    public int pop(){
+        return q.remove();
+    }
+    public int top(){
+         return q.peek();
+    }
+
+
+
+
+
+
+
+
 }
 
 /**
