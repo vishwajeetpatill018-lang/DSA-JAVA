5df05ccc550d3924a7d4627d45527b9bdc124a03
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-08-12 14:03:32 +0530

    Time: 0 ms (100%), Space: 43.3 MB (11.01%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -14,18 +14,30 @@
  * }
  */
 class Solution {
+    // HERE  , BOTH PREORDER AND POSTORDER WORKS (INORDER ONLY  WORKS WHEN SAME PARAMETER IS PAASED IN BOTH THE CALLS)
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
+
+        // For PreOrder
         // TreeNode temp = root.left;
         // root.left = root.right;
         // root.right = temp;
 
-        invertTree(root.left);
-        invertTree(root.right);
+        // invertTree(root.left);
+        // invertTree(root.right);
 
+        // For postOrder
+        // TreeNode temp = root.left;
+        // root.left = root.right;
+        // root.right = temp;
+
+
+        // Solved usinf Inorder
+        invertTree(root.left);
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
+        invertTree(root.left);
 
         return root;
     }
