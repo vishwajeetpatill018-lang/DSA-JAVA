c7a2960b6711c8e56bb448389f2fa7b21c3debd2
vishwajeetpatill018-lang <vishwajeetpatill018@gmail.com>
2026-08-14 00:34:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Root to leaf path sum/root-to-leaf-path-sum.java
@@ -0,0 +1,32 @@
+/*
+// A Binary Tree node
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+class Solution {
+    /*you are required to complete this function */
+    boolean hasPathSum(Node root, int target) {
+        
+        if(root == null){
+            return false;
+        }
+        
+        if(root.left == null && root.right == null){
+            // I am at null
+            if(target == root.data) return true;
+            else {
+                return false;
+            }
+        }
+        return hasPathSum(root.left , target-root.data ) || hasPathSum(root.right , target - root.data);
+    }
+}
